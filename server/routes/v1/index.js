07853fba54e7c6aeb2e6cf9b365a654026e153b5
ivan69 <taniazatari@Mac-Ivan.local>
2022-03-29 20:19:12 +0700

    chapter-8 react and SwaG

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,6 +1,16 @@
 const playerRouter = require("./player.routes");
 const v1 = require("express").Router();
 
+/**
+ * @swagger
+ * /api/v1:
+ *  get:
+ *    description: Use to be the entrypoint for /api/v1
+ *    responses:
+ *      200:
+ *        description: from v1
+ */
+
 v1.get("/", (_, res) => {
   res.send("from v1");
 });
@@ -8,3 +18,4 @@ v1.get("/", (_, res) => {
 v1.use("/players", playerRouter);
 
 module.exports = v1;
+
